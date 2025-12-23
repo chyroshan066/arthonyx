443f40dc96c1893fb30ef70d5f6a089571456843
Roshan <chyroshan066@gmail.com>
2025-12-23 09:35:42 +0545

    feat(dashboard): add responsive credit-card component

--- apps/dashboard/src/components/Billing.tsx
+++ apps/dashboard/src/components/Billing.tsx
@@ -1,3 +1,14 @@
 import { CreditCard } from "./Cards/CreditCard";
 
-export const Billing = () => <CreditCard />;
+export const Billing = () => (
+  <>
+    <div className="flex flex-wrap -mx-3">
+      <div className="max-w-full px-3 lg:w-2/3 lg:flex-none">
+        <div className="flex flex-wrap -mx-3">
+          <CreditCard />
+        </div>
+      </div>
+      {/* Right side of first row below this */}
+    </div>
+  </>
+);
