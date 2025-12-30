2ca397a4941d327367893df0699d210b5b52dfc9
Roshan <chyroshan066@gmail.com>
2025-12-30 19:58:00 +0545

    feat(dashboard): add responsive sidenav-type-selector component

--- apps/dashboard/src/components/layout/Configurator/Configurator.tsx
+++ apps/dashboard/src/components/layout/Configurator/Configurator.tsx
@@ -14,6 +14,7 @@ import { Button } from "../../ui/Button";
 import { SOCIAL_SHARE_BUTTONS } from "@/lib/constants";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { ConfiguratorPanelHeader } from "./ConfiguratorPanelHeader";
+import { SidenavTypeSelector } from "./SidenavTypeSelector";
 
 export const Configurator = () => {
   const dispatch = useAppDispatch();
@@ -47,45 +48,7 @@ export const Configurator = () => {
         <ConfiguratorPanelHeader />
         <Separator className="mx-0 my-1" />
         <div className="flex-auto p-6 pt-0 sm:pt-4">
-          <div className="mt-4">
-            <h6 className="mb-0">Sidenav Type</h6>
-            <p className="leading-normal text-sm">
-              Choose between 2 different sidenav types.
-            </p>
-          </div>
-          <div className="flex">
-            <Button
-              variant={sidenavType === "transparent" ? "gradient" : "outline"}
-              paddingX="4"
-              paddingY="3"
-              onClick={() => dispatch(setSidenavType("transparent"))}
-              className="w-full mb-2"
-              // className="w-full mb-2 border xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0"
-              backgroundColor={
-                sidenavType === "transparent"
-                  ? "bg-gradient-soft-purple700-pink500"
-                  : "bg-transparent"
-              }
-              btnText="Transparent"
-            />
-            <Button
-              variant={sidenavType === "white" ? "gradient" : "outline"}
-              paddingX="4"
-              paddingY="3"
-              onClick={() => dispatch(setSidenavType("white"))}
-              className="w-full mb-2 ml-2"
-              // className="w-full mb-2 ml-2 xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0
-              backgroundColor={
-                sidenavType === "white"
-                  ? "bg-gradient-soft-purple700-pink500"
-                  : "bg-transparent"
-              }
-              btnText="White"
-            />
-          </div>
-          <p className="block mt-2 leading-normal text-sm xl:hidden">
-            You can change the sidenav type just on desktop view.
-          </p>
+          <SidenavTypeSelector />
           <div className="mt-4">
             <h6 className="mb-0">Navbar Fixed</h6>
           </div>
--- apps/dashboard/src/components/layout/Configurator/SidenavTypeSelector.tsx
+++ apps/dashboard/src/components/layout/Configurator/SidenavTypeSelector.tsx
@@ -1,32 +1,55 @@
-export const SidenavTypeSelector = () => (
-  <div className="flex">
-    <Button
-      variant={sidenavType === "transparent" ? "gradient" : "outline"}
-      paddingX="4"
-      paddingY="3"
-      onClick={() => dispatch(setSidenavType("transparent"))}
-      className="w-full mb-2"
-      // className="w-full mb-2 border xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0"
-      backgroundColor={
-        sidenavType === "transparent"
-          ? "bg-gradient-soft-purple700-pink500"
-          : "bg-transparent"
-      }
-      btnText="Transparent"
-    />
-    <Button
-      variant={sidenavType === "white" ? "gradient" : "outline"}
-      paddingX="4"
-      paddingY="3"
-      onClick={() => dispatch(setSidenavType("white"))}
-      className="w-full mb-2 ml-2"
-      // className="w-full mb-2 ml-2 xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0
-      backgroundColor={
-        sidenavType === "white"
-          ? "bg-gradient-soft-purple700-pink500"
-          : "bg-transparent"
-      }
-      btnText="White"
-    />
-  </div>
-);
+import { Button } from "@/components/ui/Button";
+import {
+  selectSidebarSettings,
+  setSidenavType,
+} from "@/redux/features/configurator";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+
+export const SidenavTypeSelector = () => {
+  const dispatch = useAppDispatch();
+  const { sidenavType } = useAppSelector(selectSidebarSettings);
+
+  return (
+    <>
+      <div className="mt-4">
+        <h6 className="mb-0">Sidenav Type</h6>
+        <p className="leading-normal text-sm">
+          Choose between 2 different sidenav types.
+        </p>
+      </div>
+      <div className="flex">
+        <Button
+          variant={sidenavType === "transparent" ? "gradient" : "outline"}
+          paddingX="4"
+          paddingY="3"
+          onClick={() => dispatch(setSidenavType("transparent"))}
+          className="w-full mb-2"
+          // className="w-full mb-2 border xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0"
+          backgroundColor={
+            sidenavType === "transparent"
+              ? "bg-gradient-soft-purple700-pink500"
+              : "bg-transparent"
+          }
+          btnText="Transparent"
+        />
+        <Button
+          variant={sidenavType === "white" ? "gradient" : "outline"}
+          paddingX="4"
+          paddingY="3"
+          onClick={() => dispatch(setSidenavType("white"))}
+          className="w-full mb-2 ml-2"
+          // className="w-full mb-2 ml-2 xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-soft-purple700-pink500 xl-max:text-white xl-max:border-0
+          backgroundColor={
+            sidenavType === "white"
+              ? "bg-gradient-soft-purple700-pink500"
+              : "bg-transparent"
+          }
+          btnText="White"
+        />
+      </div>
+      <p className="block mt-2 leading-normal text-sm xl:hidden">
+        You can change the sidenav type just on desktop view.
+      </p>
+    </>
+  );
+};
